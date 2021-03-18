12b9afb801cc2890b19e65099609768788fc4b06
kimlimjustin <kimlimjustin@gmail.com>
2021-03-18 22:55:48 +0700

    Added login with github button

--- client/src/Components/auth.js
+++ client/src/Components/auth.js
@@ -22,6 +22,10 @@ const Auth = () => {
                                         <input type="password" className="input-animate" id="login-password" required autoComplete="current-password" />
                                     </div>
                                     <div className="form-group">
+                                    <button className="btn github-login-btn btn-shadow">
+                                            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                                            <span>Login with GitHub</span>
+                                        </button>
                                         <button className="btn form-control theme-adjust">Login</button>
                                     </div>
                                 </form>
@@ -47,6 +51,10 @@ const Auth = () => {
                                         </div>
                                     </div>
                                     <div className="form-group">
+                                        <button className="btn github-login-btn btn-shadow">
+                                            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                                            <span>Login with GitHub</span>
+                                        </button>
                                         <button className="btn form-control theme-adjust">Register</button>
                                     </div>
                                 </form>
